5282df96723bd00987d460194fba0d0d42589739
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-05-26 22:50:45 -0500

    tengo a little miedo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,35 +4,21 @@ import { TournamentService } from './services/tournament.service';
 import { TournamentsComponent } from './pages/tournaments/tournaments.component';
 
 const routes: Routes = [
-  // {
-  //   path: 'tournaments',
-  //   loadChildren: () => import('./modules/tournaments/tournaments.module').then(m => m.TournamentsModule)
-  // },
-
-  //  {
-  //   path: 'tournaments',
-  //   loadChildren: () => import('./modules/tournaments/tournaments.module').then(m => m.ModuleAdminModule)
-  // },
+  {
+    path: 'tournaments',
+    loadChildren: () => import('./modules/tournaments/tournaments.module').then(m => m.TournamentsModule)
+  },
 
   // {
   //   path: 'products/:id',
   //   component: TournamentService
   // },
-  {
-    path: 'admin',
-    // Aquí le dices a Angular que cargue tu módulo administrativo de forma perezosa
-    loadChildren: () => import('./admin/modules/module-admin/module-admin-routing.module').then(m => m.ModuleAdminRoutingModule)
-  },
+
   {
     path: '',
-    redirectTo: 'admin',
+    redirectTo: '/tournaments',
     pathMatch: 'full'
-  }
-  // {
-  //   path: '',
-  //   redirectTo: '/tournaments',
-  //   pathMatch: 'full'
-  // },
+  },
 
 
 
