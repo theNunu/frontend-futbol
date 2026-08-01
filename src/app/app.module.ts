1dd799b63100c87905178caada3cfe98a8d03b91
Alexis Cepeda <alexkiller0408@hotmail.com>
2026-08-01 14:13:52 -0500

    ya agarra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TournamentsComponent } from './pages/tournaments/tournaments.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TournamentsModule } from './modules/tournaments/tournaments.module';
-// import { CreateOrEditComponent } from './modules/seasons/create-or-edit/create-or-edit.component';
-// import { SeasonsModule } from './modules/seasons/seasons.module';
 import { NewsModule } from './modules/news/news.module';
+import { SeasonsTournamentModule } from './modules/seasons-tournament/seasons-tournament.module';
+
 
 import { providePrimeNG } from 'primeng/config'; // Importación de PrimeNG v19
 import Aura from '@primeng/themes/aura'; // Tema visual moderno de PrimeNG v19
+
+// Importa el módulo específico del componente que deseas usar
+import { ButtonModule } from 'primeng/button';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 @NgModule({
   declarations: [
     AppComponent,
     TournamentsComponent,
-    // CreateOrEditComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule, // Asegúrate de que esté aquí para activar los estilos dinámicos
     TournamentsModule,
-    // SeasonsModule,
-    BrowserAnimationsModule,
-    NewsModule
+    NewsModule,
+    SeasonsTournamentModule,
+    ButtonModule // Cargas el módulo del componente aquí para que esté disponible
   ],
   providers: [
+
+    provideAnimationsAsync(), // Requerido para animaciones de componentes
     // Aquí es donde PrimeNG v19 inyecta los estilos automáticamente en arquitecturas de módulos
     providePrimeNG({
       theme: {
--- src/app/modules/seasons-tournament/seasons-tournament.module.ts
+++ src/app/modules/seasons-tournament/seasons-tournament.module.ts
@@ -7,23 +7,11 @@ import { CreateSeasonComponent } from './pages/create-season/create-season.compo
 import { UpdateSeasonComponent } from './pages/update-season/update-season.component';
 import { FormSeasonsComponent } from './components/form-seasons/form-seasons.component';
 
-// import { TableModule } from 'primeng/table'; // 👈 1. Importación obligatoria
-import { TableModule, Table } from 'primeng/table';
+import { TableModule } from 'primeng/table';
 
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field'; // <-- Para <mat-form-field>
-import { MatInputModule } from '@angular/material/input';         // <-- Para el input con matInput
-import { MatDatepickerModule } from '@angular/material/datepicker'; // <-- Para el calendario
-import { MatNativeDateModule } from '@angular/material/core';     // <-- Para que el calendario entienda fechas nativas
 import { FormsModule } from '@angular/forms'; // 1. Importa el módulo
-
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { InputTextModule } from 'primeng/inputtext';
-import { SelectModule } from 'primeng/select';
-// import { ListNewsComponent } from './pages/list-news/list-news.component';
-// import { CreateNewsComponent } from './pages/create-news/create-news.component';
-// import { UpdateNewsComponent } from './pages/update-news/update-news.component'; 
+// 1. Importa el módulo de PrimeNG aquí arriba
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -37,23 +25,7 @@ import { SelectModule } from 'primeng/select';
     SeasonsTournamentRoutingModule,
     FormsModule,
     TableModule,
-    Table
-    // Table
-    // MatTableModule,
-    // //     // Módulos de Angular Material
-    // MatPaginatorModule,
-    // MatFormFieldModule,
-    // MatInputModule,
-    // MatButtonModule,
-    // MatDialogModule,
-    // MatDatepickerModule,
-    // MatNativeDateModule,
-
-    // //para uso de primeng (buscador de noticia)
-    // IconFieldModule,
-    // InputIconModule,
-    // InputTextModule,
-    // SelectModule
+    ButtonModule // 2. Agrégalo aquí para que ListSeasonComponent pueda usarlo
   ]
 })
 export class SeasonsTournamentModule { }
